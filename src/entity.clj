f301533afb3b1b471ebdc061ed22133f208014f7
aatack <alexanderjtatack@gmail.com>
2023-11-03 02:31:09 +0000

    Document the entity protocol

--- src/entity.clj
+++ src/entity.clj
@@ -1,9 +1,18 @@
-(ns entity 
+(ns entity
   (:require [clojure.set :as set]))
 
 (defprotocol Entity
-  (dependencies [this])
-  (cursor? [this]))
+  "Describes any value that is derived in some way from a fixed state object.
+   
+   Entities track which paths within the state object they are derived from.  The set
+   of dependencies describes the set of paths within the state which, should any of
+   their values change, might cause the value of the entity to also change."
+
+  (dependencies [this]
+    "Return the paths of all entities that are dependencies of this entity.")
+
+  (lookup? [this]
+    "Determine whether or not this is a direct lookup from the underlying state."))
 
 (defrecord Database [value]
   clojure.lang.IDeref
@@ -11,17 +20,17 @@
 
   Entity
   (dependencies [_] #{})
-  (cursor? [_] true))
+  (lookup? [_] true))
 
 (defrecord Lookup [root path]
   clojure.lang.IDeref
   (deref [_] (get-in @root path))
 
   Entity
-  (dependencies [this] (if (cursor? this)
+  (dependencies [this] (if (lookup? this)
                          (set (map #(concat % path) (dependencies root)))
                          (dependencies root)))
-  (cursor? [_] (cursor? root)))
+  (lookup? [_] (lookup? root)))
 
 (defrecord Derive [function arguments]
   clojure.lang.IDeref
@@ -29,4 +38,4 @@
 
   Entity
   (dependencies [_] (apply set/intersection (map dependencies arguments)))
-  (cursor? [_] false))
+  (lookup? [_] false))
