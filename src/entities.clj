c6760ff8561791ee685bc8f964f91a3a41cb6427
aatack <alexanderjtatack@gmail.com>
2023-11-11 02:30:13 +0000

    Define an API for rendering child entities

--- src/entities.clj
+++ src/entities.clj
@@ -7,8 +7,11 @@
 (defrecord Consume [context-key workspace-key entity]
   Entity
   (manage [_ context arguments workspace]
+    ;; (TODO) Transfer input dependencies and define output dependencies conditionally
     (manage entity context arguments
             (-> workspace
                 (assoc workspace-key
                        (get context context-key))
                 (conj-dependency :context context-key)))))
+
+(defrecord Child [key definition bindings])
