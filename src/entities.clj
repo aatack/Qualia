5d1023e69ba44b8c0894a2c5c93d9e6489332b90
aatack <alexanderjtatack@gmail.com>
2023-11-18 23:52:08 +0000

    Update the interface for entities

--- src/entities.clj
+++ src/entities.clj
@@ -2,18 +2,11 @@
   (:require [dependencies :refer [conj-dependency]]))
 
 (defprotocol Entity
-  (manage [this context arguments workspace]))
+  (manage [this scope changes]))
 
 (defrecord Consume [context-key workspace-key entity]
   Entity
-  (manage [_ context arguments workspace]
-    ;; (TODO) Transfer input dependencies and define output dependencies conditionally
-    (manage entity
-            context
-            arguments
-            (-> workspace
-                (assoc workspace-key
-                       (get context context-key))
-                (conj-dependency :context context-key)))))
+  (manage [_ scope changes]
+    (throw (Exception. "Not implemented"))))
 
 (defrecord Child [key definition bindings])
