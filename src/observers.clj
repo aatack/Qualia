916bb2da7b5bb247362c98b9bed1994fc9ec2265
aatack <alexanderjtatack@gmail.com>
2023-11-21 14:42:52 +0000

    Create a rough implementation of state management

--- src/observers.clj
+++ src/observers.clj
@@ -9,3 +9,11 @@
   (manage [_ scope _]
     {:value (get-in scope path)
      :dependencies (conj-dependency nil path)}))
+
+(defrecord State [path default observer]
+  Observer
+  (manage [_ scope changes]
+    (manage observer
+            (update-in scope (apply vector :state path)
+                       #(or % (:value (manage default scope changes))))
+            changes)))
