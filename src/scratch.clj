6a991cb10b7ae8fe115cbc63d549040fd7b13543
aatack <alexanderjtatack@gmail.com>
2024-06-07 01:37:16 +0100

    Have a stab at wrapping the whole thing in an atom

--- src/scratch.clj
+++ src/scratch.clj
@@ -7,20 +7,36 @@
         (swap! cache assoc key (head)))
       (apply (get @cache key) body))))
 
-(defn example-child []
+(defn watch [mount & arguments]
+  (let [children (atom {})
+        inner-watch (fn [key child-mount & child-arguments]
+                      ; This does not yet track which arguments the child was most
+                      ; recently applied to
+                      (when-not (contains? @children key)
+                        (swap! children assoc key
+                               (apply watch child-mount child-arguments)))
+                      (get @children key))
+        render (mount inner-watch)
+        state (atom (apply render arguments))]
+    (doall
+     (for [dep (:deps @state)]
+       (add-watch dep :k (fn [& args] (reset! state (apply render arguments))))))
+    state))
+
+(defn example-child [w]
   (let [count (atom 0)]
     (fn [target]
       {:value (str "Target " target " count " @count)
        :handle (fn [key]
                  (when (= key target)
-                   (swap! count inc)))})))
+                   (swap! count inc)))
+       :deps [count]})))
 
-(defn example []
-  (let [letters (atom [])
-        watch (watcher)]
+(defn example [w]
+  (let [letters (atom [])]
     (fn []
       (let [children (map (fn [letter]
-                            (watch letter example-child letter))
+                            (w letter example-child letter))
                           @letters)]
         {:value (str "Count: " @letters (into [] (map :value children)))
          :handle (fn [key]
@@ -31,11 +47,19 @@
 
 (comment
 
+  (def x (watch example-child "c"))
+
+  ((:handle @x) "c")
+
+  @x
+
+  @(reactive-atom example-child "a")
+
   (def app (example))
 
   (def c (example-child))
 
-  (app)
+  (:value (app))
 
   (c "f")
 
