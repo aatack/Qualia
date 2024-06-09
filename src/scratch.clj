745183cdfb446d7b5b93ff1247ad90d2d476d4d9
aatack <alexanderjtatack@gmail.com>
2024-06-09 19:06:54 +0100

    Move cursors to a different file

--- src/scratch.clj
+++ src/scratch.clj
@@ -1,25 +1,5 @@
-(ns scratch)
-
-(defrecord Cursor [a k]
-  clojure.lang.IDeref
-  (deref [_]
-    (get @a k))
-
-  clojure.lang.IAtom
-  (reset [_ new-value]
-    (get (swap! a assoc k new-value) k))
-
-  (swap [_ f]
-    (get (swap! a update k f) k))
-  (swap [_ f x]
-    (get (swap! a update k f x) k))
-  (swap [_ f x y]
-    (get (swap! a update k f x y) k))
-  (swap [_ f x y more]
-    (get (swap! a update k #(apply f % x y more)) k)))
-
-(defn cursor [a k]
-  (Cursor. a k))
+(ns scratch 
+  (:require [state :refer [cursor]]))
 
 (defn mount [component context]
   (let [value (cursor context :value)
---
+++ src/state.clj
@@ -0,0 +1,22 @@
+(ns state)
+
+(defrecord Cursor [a k]
+  clojure.lang.IDeref
+  (deref [_]
+    (get @a k))
+
+  clojure.lang.IAtom
+  (reset [_ new-value]
+    (get (swap! a assoc k new-value) k))
+
+  (swap [_ f]
+    (get (swap! a update k f) k))
+  (swap [_ f x]
+    (get (swap! a update k f x) k))
+  (swap [_ f x y]
+    (get (swap! a update k f x y) k))
+  (swap [_ f x y more]
+    (get (swap! a update k #(apply f % x y more)) k)))
+
+(defn cursor [a k]
+  (Cursor. a k))
