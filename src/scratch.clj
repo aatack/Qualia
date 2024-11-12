4ddf74f55aed6ce43351c88fb7e0cff4bd1d68e4
aatack <alexanderjtatack@gmail.com>
2024-11-12 15:45:32 +0000

    Leave a note on disallowing overlapping entities without a nested call

--- src/scratch.clj
+++ src/scratch.clj
@@ -41,6 +41,12 @@
     ()))
 
 (defn q-entity [builder]
+  ;; Consider tracking arguments in the ephemeral state and asserting that they are nil
+  ;; here; then we know the entity wrapper is only called once for each time it's
+  ;; overridden by a separate nested call.  Having wrapped entities should generally be
+  ;; illegal, since much of the setup of the state map is designed around an individual
+  ;; entity
+
   (fn [&arguments]
     ;; Pruning could be done at this point if needed
     (fn [state updates context queue-update]
