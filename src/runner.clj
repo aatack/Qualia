5fd90af6538d0190b14f0b93b57e2df480d73712
aatack <alexanderjtatack@gmail.com>
2024-11-17 21:18:36 +0000

    Make functions for flushing runners

--- src/runner.clj
+++ src/runner.clj
@@ -6,17 +6,17 @@
    :builder builder})
 
 (defn- build-queue-update [runner]
-  (fn [path key function]
-    (swap! (:updates runner)
-           (fn [current]
-             (update-in current [path key] #(conj (or % []) function))))))
+  {:path []
+   :function (fn [path key function]
+               (swap! (:updates runner)
+                      (fn [current]
+                        (update-in current [path key] #(conj (or % []) function)))))})
 
 (defn flush-runner! [runner]
-  (let [updates @(:updates runner)
-        queue-update {:path []
-                      :function (fn [path key function]
-                                  (swap! (:updates runner)
-                                         (fn [current])))}]
+  (let [updates @(:updates runner)]
     (reset! (:updates runner) {})
     (swap! (:state runner)
-           (fn [state] ((:builder runner) state updates {} queue-update)))))
+           (fn [state] ((:builder runner) state
+                                          updates
+                                          {}
+                                          (build-queue-update runner))))))
