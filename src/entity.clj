ce78c1f72bd8a771536abbde31a8fc735fae7da8
aatack <alexanderjtatack@gmail.com>
2024-11-17 14:46:01 +0000

    Begin implementing pruning logic

--- src/entity.clj
+++ src/entity.clj
@@ -1,20 +1,21 @@
 (ns entity)
 
 (defn q-entity [builder]
-  ;; Consider tracking arguments in the ephemeral state and asserting that they are nil
-  ;; here; then we know the entity wrapper is only called once for each time it's
-  ;; overridden by a separate nested call.  Having wrapped entities should generally be
-  ;; illegal, since much of the setup of the state map is designed around an individual
-  ;; entity
-
   (fn [& arguments]
-    ;; Pruning could be done at this point if needed
     ^{::type ::entity}
     (fn [state updates context queue-update]
-      ((apply builder arguments) (assoc state :arguments arguments)
-                                 updates
-                                 context
-                                 queue-update))))
+      (let [arguments-changed? true
+            has-updates? true
+            context-changed? true]
+        (if (or arguments-changed? has-updates? context-changed?)
+          ((apply builder arguments) (assoc state :arguments arguments)
+                                     updates
+                                     context
+                                     queue-update)
+          ;; If the arguments haven't changed, there are no updates targeting this sub-
+          ;; tree, and none of the consumed contextual values have changed, there is no
+          ;; need to re-render this entity
+          state)))))
 
 (comment
   (require '[literal :refer [q-literal]]
