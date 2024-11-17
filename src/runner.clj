7ffcdac5965ff58f9c89763060186859f85a9cdc
aatack <alexanderjtatack@gmail.com>
2024-11-17 22:24:06 +0000

    Allow direct dereferencing of runners

--- src/runner.clj
+++ src/runner.clj
@@ -1,38 +1,34 @@
 (ns runner)
 
-(defn build-runner
-  ([builder limit]
-   {:updates (atom {})
-    :state (atom {})
-    :builder builder
-    :limit limit})
-  ([builder]
-   (build-runner builder 10)))
-
 (defn- build-queue-update [runner]
   {:path []
    :function (fn [path key function]
-               (swap! (:updates runner)
+               (swap! (.updates runner)
                       (fn [current]
                         (update-in current [path key] #(conj (or % []) function)))))})
 
 (defn flush-runner! [runner]
-  (let [updates @(:updates runner)]
-    (reset! (:updates runner) {})
-    (swap! (:state runner)
-           (fn [state] ((:builder runner) state
+  (let [updates @(.updates runner)]
+    (reset! (.updates runner) {})
+    (swap! (.state runner)
+           (fn [state] ((.builder runner) state
                                           updates
                                           {}
                                           (build-queue-update runner))))))
 
-(defn deref-runner! [runner]
-  (loop [tries 0]
-    (if (or (and (contains? @(:state runner) :value) (= 0 (count @(:updates runner))))
-            (> tries (:limit runner)))
-      (:value @(:state runner))
-      (do (flush-runner! runner)
-          (recur (inc tries))))))
+(deftype Runner [updates state builder limit]
+  clojure.lang.IDeref
+  (deref [this]
+    (loop [tries 0]
+      (if (or (and (contains? @state :value) (= 0 (count @updates)))
+              (> tries limit))
+        (:value @state)
+        (do (flush-runner! this)
+            (recur (inc tries)))))))
 
+(defn build-runner
+  ([builder limit] (Runner. (atom {}) (atom {}) builder limit))
+  ([builder] (build-runner builder 10)))
 
 (comment
   (require '[builders.literal :refer [q-literal]]
@@ -51,14 +47,14 @@
   (let [runner (build-runner (counter))]
 
     ;; The entity should initialise automatically
-    (assert (= 0 (:count (deref-runner! runner))))
+    (assert (= 0 (:count @runner)))
 
     ;; Flushing/dereferencing the runner when there are no changes should have no effect
-    (assert (= 0 (:count (deref-runner! runner))))
+    (assert (= 0 (:count @runner)))
 
     ;; Incrementing the counter should correctly update the state
-    ((:inc (deref-runner! runner)))
-    (assert (= 1 (:count (deref-runner! runner)))))
+    ((:inc @runner))
+    (assert (= 1 (:count @runner))))
 
   (def nested-counter
     (q-entity (fn [] (q-nested {:a (counter)}
@@ -67,13 +63,13 @@
   (let [runner (build-runner (nested-counter))]
     ;; Calling a state update multiple times without flushing should still yield valid
     ;; results
-    (let [increment (:inc (deref-runner! runner))]
+    (let [increment (:inc @runner)]
       (increment)
       (increment)
       (increment))
 
     ;; Internal updates still work on nested entities
-    (assert (= 3 (:count (deref-runner! runner)))))
+    (assert (= 3 (:count @runner))))
 
   (def doubly-nested-counter
     (q-entity (fn [] (q-nested {:b (nested-counter)}
@@ -82,12 +78,12 @@
   (let [runner (build-runner (doubly-nested-counter))]
     ;; Calling a state update multiple times without flushing should still yield valid
     ;; results
-    (let [increment (:inc (deref-runner! runner))]
+    (let [increment (:inc @runner)]
       (increment)
       (increment))
 
     ;; Internal updates still work on *doubly* nested entities
-    (assert (= 2 (:count (deref-runner! runner)))))
+    (assert (= 2 (:count @runner))))
 
   (def broken-counter
     (q-entity (fn []
@@ -103,4 +99,4 @@
   (let [runner (build-runner (broken-counter))]
 
     ;; After cycling ten times, the limit should kick in and prevent an infinite loop
-    (assert (= 10 (:count (deref-runner! runner))))))
+    (assert (= 10 (:count @runner)))))
