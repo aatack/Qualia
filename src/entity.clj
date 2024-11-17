2c808e1fdf7d12c97e43d1440f692f905392fb69
aatack <alexanderjtatack@gmail.com>
2024-11-17 14:35:59 +0000

    Add more entity tests

--- src/entity.clj
+++ src/entity.clj
@@ -30,4 +30,42 @@
                (q-internal
                 {:x x :y y}
                 (fn [values] (q-literal (str @(:x values) ", " @(:y values)))))))]
-        ((example 1 2) {} {} {} (fn []))))))
+        ((example 1 2) {} {} {} (fn [])))))
+
+  (assert ;; Rendering multiple times with no updates does not change the state
+   (= {:arguments '(1 2)
+       :internal {:x 1 :y 2}
+       :value "1, 2"}
+      (let [example
+            (q-entity
+             (fn [x y]
+               (q-internal
+                {:x x :y y}
+                (fn [values] (q-literal (str @(:x values) ", " @(:y values)))))))
+  
+            after-first-render
+            ((example 1 2) {} {} {} (fn []))
+  
+            after-second-render
+            ((example 1 2) after-first-render {} {} (fn []))]
+  
+        after-second-render)))
+  
+  (assert ;; Rendering with updates correctly propagates changes to the internal state
+   (= {:arguments '(1 2)
+       :internal {:x 2 :y 1}
+       :value "2, 1"}
+      (let [example
+            (q-entity
+             (fn [x y]
+               (q-internal
+                {:x x :y y}
+                (fn [values] (q-literal (str @(:x values) ", " @(:y values)))))))
+  
+            after-first-render
+            ((example 1 2) {} {} {} (fn []))
+  
+            after-second-render
+            ((example 1 2) after-first-render {() {:x [inc] :y [dec]}} {} (fn []))]
+  
+        after-second-render))))
