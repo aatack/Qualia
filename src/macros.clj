02f304bc45334720d45c377d7b8566bd7a58b3c5
aatack <alexanderjtatack@gmail.com>
2024-11-18 00:07:54 +0000

    Introduce a similar macro for internals

--- src/macros.clj
+++ src/macros.clj
@@ -1,8 +1,27 @@
 (ns macros
   (:require
    [builders.entity :refer [q-entity]]
+   [builders.internal :refer [q-internal]]
    [builders.literal :refer [q-literal]]
-   [runner :refer [build-runner]]))
+   [helpers :refer [void-update]]))
 
 (defmacro defentity [name args & body]
   `(def ~name (~q-entity (fn ~args ~@body))))
+
+(defmacro let-internal [bindings & body]
+  (let [internals (gensym "internal")
+
+        inbound-bindings
+        (->> bindings
+             (partition 2)
+             (map (fn [[key value]] [(keyword key) value]))
+             (into {}))
+
+        outbound-bindings
+        (->> inbound-bindings
+             (mapcat (fn [[key _]] [(symbol key) `(~key ~internals)]))
+             (into []))]
+
+    `(~q-internal ~inbound-bindings
+                  (fn [~internals]
+                    (let ~outbound-bindings ~@body)))))
