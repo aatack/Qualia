e5a66a28047b35964b287143ee92ddcb9505c5fc
aatack <alexanderjtatack@gmail.com>
2024-12-01 17:59:45 +0000

    Update dependency maps when the dependencies change

--- src/entities.clj
+++ src/entities.clj
@@ -1,11 +1,27 @@
 (ns entities)
 
-(defn watch-entity! [key reference old-state new-state]
+(defn watch-entity! [_ entity old-state new-state]
   ;; When the validity transitions from true to false, invalidate all dependents of this
   ;; entity
-  (when (and (:valid old-state) (not (:valid new-state))) 
+  (when (and (:valid old-state) (not (:valid new-state)))
     (doseq [dependent (:dependents new-state)]
-      (swap! dependent assoc :valid false))))
+      (swap! dependent assoc :valid false)))
+
+  (let [old-dependencies (-> old-state :dependencies keys set)
+        new-dependencies (-> old-state :dependencies keys set)
+
+        removed-dependencies (reduce disj old-dependencies new-dependencies)
+        added-dependencies (reduce disj new-dependencies old-dependencies)]
+
+    ;; If any dependencies are no longer dependencies, remove this entity from their
+    ;; list of dependents
+    (doseq [dependency removed-dependencies]
+      (swap! dependency update :dependents disj entity))
+
+    ;; If any new dependencies have appeared, add this entity to their list of
+    ;; dependents
+    (doseq [dependency added-dependencies]
+      (swap! dependency update :dependents conj entity))))
 
 (defn build-entity [function & arguments]
   (let [entity (atom {:function function
