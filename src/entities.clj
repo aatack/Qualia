50dbeb2e7581e58f6a98972d48d8e73218048daf
aatack <alexanderjtatack@gmail.com>
2024-12-06 01:09:15 +0000

    Copy over the watcher

--- src/entities.clj
+++ src/entities.clj
@@ -51,6 +51,29 @@
 
       value)))
 
+(defn- watch-state [_ entity old-state new-state]
+  ;; When the validity transitions from true to false, invalidate all dependents of this
+  ;; entity
+  (when (and (:valid old-state) (not (:valid new-state)))
+    (doseq [dependent (:dependents new-state)]
+      (swap! dependent assoc :valid false)))
+
+  (let [old-dependencies (-> old-state :dependencies keys set)
+        new-dependencies (-> new-state :dependencies keys set)
+
+        removed-dependencies (reduce disj old-dependencies new-dependencies)
+        added-dependencies (reduce disj new-dependencies old-dependencies)]
+
+    ;; If any dependencies are no longer dependencies, remove this entity from their
+    ;; list of dependents
+    (doseq [dependency removed-dependencies]
+      (swap! dependency update :dependents disj entity))
+
+    ;; If any new dependencies have appeared, add this entity to their list of
+    ;; dependents
+    (doseq [dependency added-dependencies]
+      (swap! dependency update :dependents conj entity))))
+
 (comment
   (def e (Entity. identity (atom {:arguments [nil '(1)]})))
 
